a049223d505fd4681f4640cbf16e3a5c7efe62f4
oropm-24 <oropm-24@lily.rhodes.edu>
2023-03-09 23:33:07 -0600

    submitting Kata04

--- exercise04/reverse.c
+++ exercise04/reverse.c
@@ -10,21 +10,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 500
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  int numbers[MAX];   // numbers= array
-  int num, count = 0; // count = how many in array; num = placeholder
+  int max = 10; // buffer array of 10
+  int size = 0;
+  int *arr = malloc(max * sizeof(int));
+  char buffer_array[100];
 
-  while (scanf("%d", &num) != EOF && count < MAX) {
-    numbers[count++] = num; // continues adding to count and num being put in
-                            // numbers
+  while (fgets(buffer_array, 100, stdin) != NULL) { // fills array
+    int num = atoi(buffer_array);
+    arr[size] = num;
+    size++; // keeps track of how big array is while also filling
+
+    if (size == max) {                       // knows whether to add space
+      max *= 2;                              // if array is full it doubles
+      arr = realloc(arr, max * sizeof(int)); // reallocates space, multiplies
+                                             // max times size of int to get
+                                             // bytes required
+    }
   }
-
-  for (int i = count - 1; i >= 0; i--) { // starts from back and creates
-    printf("%d ", numbers[i]);           // d can take any size decimal
+  // prints reversed array
+  for (int i = size - 1; i >= 0; i--) {
+    printf("%d ", arr[i]);
   }
-  printf("\n");
+  printf("\n"); // so that command prompt not on same line
 
+  free(arr); // frees space after
   return 0;
 }
